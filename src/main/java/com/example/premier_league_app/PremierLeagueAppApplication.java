b713cf2d6a49f0690bfccf024e4664190bf18518
Mr Sharma <MrSharma@example.com>
2025-12-01 22:05:12 +0530

    proj init

---
+++ src/main/java/com/example/premier_league_app/PremierLeagueAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.premier_league_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PremierLeagueAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PremierLeagueAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/premier_league_app/PremierLeagueAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.premier_league_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PremierLeagueAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
